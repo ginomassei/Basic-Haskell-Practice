864f1df38f9e6e553aaa67b980864a7ca1de6379
Gino Massei <ginomassei@Ginos-MacBook-Pro.local>
2020-10-13 11:26:06 -0300

    Nais

--- main3.hs
+++ main3.hs
@@ -1,14 +1,14 @@
-{- Alumnos: 
-Massei, Gino - 79366
-Perlo, Matías - 83723
-Laciar, Ignacio - 71348
-Gonzales, Facundo - 77494
--}
-
 -- AUX Functions.
 fact :: Integer -> Integer
 fact 0 = 1  -- End of recursion.
 fact n = n * (fact (n - 1))
+
+sin' :: Float -> Integer -> Float
+sin' x 0 = x  -- End of recursion.
+sin' x n
+    | x >= 0 && x <= 6.284
+        = ((-1) ^ n / fromIntegral (fact (2*n + 1))) * (x ^ (2*n + 1)) + sin' x (n - 1)
+    | otherwise = 2
 
 -- TP Functions.
 funcion1 :: Integer -> Integer -> Integer
@@ -25,10 +25,8 @@ funcion3 :: Float -> Integer -> Float
 funcion3 _x 0 = 1  -- End of recursion.
 funcion3 x n = x ^ n / fromIntegral (fact n) + funcion3 x (n - 1)
 
-funcion4:: Float -> Float
-funcion4 x
-    | x < 0 || x > 6.284 = 2
-    | otherwise  = sum [((-1) ^ n / fromIntegral (fact (2*n + 1))) * (x ^ (2*n + 1)) | n <- [0..9]]
+funcion4 :: Float -> Float
+funcion4 x = sin' x 10
 
 funcion5 :: [Float] -> [Float]
 funcion5 list =
